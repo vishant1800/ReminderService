4e68ca1f2ef46461c925dabe2a45c7985debe18c
Vishant Niboray <vishantniboray1800@gmail.com>
2024-10-04 16:10:19 +0530

    Setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig')
+const { PORT } = require('./config/serverConfig')
+
+const { sendBasicEmail } = require('./services/email-service')
 
 const app = express();
 
-const startServer = async() => {
+const startServer = async () => {
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, () => {
-        console.log(`Server started at port, ${PORT}`)
+        console.log(`Server started at port, ${PORT}`);
+
+        sendBasicEmail(
+            'vishant0426@gmail.com',  //from will not change, it will the email used in auth
+            'moneyniboray1246@gmail.com',
+            'This is a testing email',
+            'Hey, How are you? I hope you are satisfied with the company support'
+        );
+
     })
 }
 
---
+++ src/services/email-service.js
@@ -0,0 +1,19 @@
+const sender = require('../config/emailConfig')
+
+const sendBasicEmail = async(mailFrom, mailTo, mailSubject, mailBody) => {
+    try {
+        await sender.sendMail({
+            from: mailFrom,
+            to: mailTo,
+            subject: mailSubject,
+            text: mailBody
+        })
+        //console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    sendBasicEmail
+}
